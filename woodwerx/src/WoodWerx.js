e387bff1c65b07ff65cd54c6ef442928cb5e6dfe
Blake Davis <blavis21@gmail.com>
2019-07-30 08:39:58 -0500

    navbar is hidden on login/register pages only

--- woodwerx/src/WoodWerx.js
+++ woodwerx/src/WoodWerx.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import Nav from './components/navbar/Nav'
+import Login from './components/Authentication/Login'
 import AppViews from './components/AppViews';
 
 export default class App extends Component {
+
+  state = {
+    authenticated: sessionStorage.getItem("user")
+  }
+
+  setAuthState = () => {
+    if (sessionStorage.getItem("user")) {
+      this.setState({ authenticated: true })
+    } else {
+      this.setState({ authenticated: false })
+    }
+  }
   render() {
-    return (
-      <React.Fragment>
-        <Nav />
-        <AppViews />
-      </React.Fragment>
-    )
+    if (this.state.authenticated) {
+      return (
+        <React.Fragment>
+          <Nav />
+          <AppViews isAuthenticated={this.state.authenticated} />
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <Login setAuthState={this.setAuthState} />
+      )
+    }
   }
 }
 
